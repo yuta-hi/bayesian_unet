cce1dbd75fad9cc29b77eb1c76b33c6a3eb0ffa6
yuta-hi <hiasa.yuta.ht7@is.naist.jp>
2020-07-04 22:50:15 +0900

    add logging utility

--- chainer_bcnn/utils/__init__.py
+++ chainer_bcnn/utils/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import os
 import numpy as np
 import contextlib
+import datetime
 import warnings
 
 @contextlib.contextmanager
@@ -51,3 +52,34 @@ def find_latest_snapshot(fmt, path, return_fullpath=True):
         return os.path.join(path, ret)
 
     return ret
+
+
+def get_git_revision(base_path='./', short=True):
+    # https://stackoverflow.com/questions/14989858/get-the-current-git-hash-in-a-python-script
+    git_dir = os.path.join(base_path, '.git')
+    with open(os.path.join(git_dir, 'HEAD'), 'r') as head:
+        ref = head.readline().split(' ')[-1].strip()
+
+    with open(os.path.join(git_dir, ref), 'r') as git_hash:
+        revision = git_hash.readline().strip()
+
+    if short:
+        return revision[:7]
+
+    return revision
+
+
+def get_logstamp(delimiter='_'):
+
+    now = datetime.datetime.now()
+
+    logstamp = []
+    logstamp.append(now.strftime('%y%m%d'))
+    logstamp.append(now.strftime('%H%M%S'))
+
+    try:
+        logstamp.append(get_git_revision())
+    except Exception:
+        pass
+
+    return delimiter.join(logstamp)
